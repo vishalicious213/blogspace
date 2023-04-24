f64f563c7ccfe1415f8a1e9eab82c07eaa806807
vishalicious213 <vishalicious213@gmail.com>
2023-04-24 10:31:50 -0700

    got first 5 posts and rendered them onscreen

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// get first 5 posts and render them
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(posts => {
         const newPosts = posts.slice(0, 5)
-        console.log(newPosts)
+        let postsHtml = ""
+        newPosts.forEach(function(post) {
+            postsHtml += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        })
+        document.getElementById("blog-posts").innerHTML = postsHtml
     })
