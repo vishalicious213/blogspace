2ed3239d042238000d7c12e1fc7a2549e707b0e3
vishalicious213 <vishalicious213@gmail.com>
2023-04-24 17:57:05 -0700

    turned posts from fetch() into a global array

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+let postsArray = []
+
 // get first 5 posts and render them
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(posts => {
-        const newPosts = posts.slice(0, 5)
+        postsArray = posts.slice(0, 5)
         let postsHtml = ""
-        newPosts.forEach(function(post) {
+        postsArray.forEach(function(post) {
             postsHtml += `
                 <h3>${post.title}</h3>
                 <p>${post.body}</p>
